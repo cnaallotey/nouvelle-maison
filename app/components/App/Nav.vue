a76cd23b6e70fbc8968ab478e9a86ddece269b3f
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2025-12-10 09:43:10 +0000

    update

--- app/components/App/Nav.vue
+++ app/components/App/Nav.vue
@@ -2,7 +2,7 @@
     <div class="w-full px-4 md:px-10 py-4 md:py-10 bg-black/80 backdrop-blur-3xl">
     <div class="w-full flex flex-row-reverse md:flex-row item-center justify-between gap-3 md:gap-10">
     <button class="text-white font-semibold text-2xl p-4 bg-black/50" @click="showMenu = true"><span class="sr-only">Menu</span><PhList class="w-5 h-5 text-yellow-500" /></button>
-    <div class="flex items-center gap-3"><img src="/images/logo-white.png" class="h-12 md:h-20" alt="logo"><span class="text-white font-semibold text-sm uppercase sr-only tracking-wider">Nouvelle <br class="md:hidden"> Maison</span></div>
+    <div class="flex items-center gap-3"><img src="/images/logo-white.png" class="h-12 md:h-20 shrink-0 md:absolute md:inset-0 md:m-auto" alt="logo"><span class="text-white font-semibold text-sm uppercase sr-only tracking-wider">Nouvelle <br class="md:hidden"> Maison</span></div>
     <AppButton @click="$router.push('/#contact')" class="hidden md:block">Contact Us</AppButton>
     </div>
 
